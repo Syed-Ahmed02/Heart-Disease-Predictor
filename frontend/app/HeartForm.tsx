35e746ab8a600bae476dacd388f3dc8b724ff960
Syed-Ahmed02 <syedahmedd.02@gmail.com>
2025-07-24 16:26:09 -0400

    updated api request to use env variables in frontend

--- frontend/app/HeartForm.tsx
+++ frontend/app/HeartForm.tsx
@@ -83,7 +83,7 @@ export default function HeartForm() {
     setResult(null);
     setError(null);
     try {
-      const response = await fetch("http://127.0.0.1:8000/classify_heart_disease", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/classify_heart_disease`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
